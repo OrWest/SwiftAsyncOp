fd8ecb4b2d19c186944a1627003f417e2631cbc5
Alexander Motarykin <wwweshka@rambler.ru>
2021-02-01 20:46:39 +0300

    Commented tests.

--- Tests/SwiftAsyncOpTests/SwiftAsyncOpTests.swift
+++ Tests/SwiftAsyncOpTests/SwiftAsyncOpTests.swift
@@ -6,7 +6,7 @@ final class SwiftAsyncOpTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftAsyncOp().text, "Hello, World!")
+        //XCTAssertEqual(SwiftAsyncOp().text, "Hello, World!")
     }
 
     static var allTests = [
